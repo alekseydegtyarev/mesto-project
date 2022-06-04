755aa8a1121cb6a5c1f6963c42cd9ad1c2b3c0d9
alekseydegtyarev <alekseydegtyarev41rus@gmail.com>
2022-06-05 04:00:04 +1200

    практически полностью переделан script.js убраны дублирующие объявления переменных из функций и перенесены в начало файла, исправлены функции открытия и закрытия попапа для получения возможности их реиспользования, переделана функция добавления карточек (пример в тз только сбивает с толку) теперь функция изначально используется для начальных карточек из массива и реиспользуется при добавлении новых, добавлены лайк, удаление и попап для зума картинки, отправка на первое ревью

--- script.js
+++ script.js
@@ -1,159 +1,4 @@
-const profileBtnEdit = document.querySelector('.profile__button-edit'), // кнопка редактирования профиля
-    popupProfile = document.querySelector('.popup__profile'), //окно редактирования профиля
-    btnCloseProfile = document.querySelector('.popup__close_profile'), //кнопка закрытия окна редактирования профиля
-    popupPlace = document.querySelector('.popup__new-place'), //попап окна добавления "мест"
-    btnClosePlace = document.querySelector('.popup__close_place'); //кнопка закрытия окна добавления мест
-    btnCloseZoom = document.querySelector('.popup__close_img-zoom') //кнопка закрытия увеличенной картинки
-    cardImg = document.querySelector('.cards__image');
-
-// функция открытия поп-апа редактирования профиля
-function profileEdit() {
-  popupProfile.classList.add('popup_opened'); //добавляем класс, чтобы попап был виден
-//кладём в инпуты текущие значения со страницы
-  let nameInput = document.querySelector('.popup__input-edit_name'), //присваиваем
-    jobInput = document.querySelector('.popup__input-edit_about'), //переменные
-    name = document.querySelector('.profile__name'), //элементам
-    job = document.querySelector('.profile__about'); //страницы
-//в "значение"(value) инпутов кладём текущий текст со страницы
-  nameInput.value = name.textContent;
-  jobInput.value = job.textContent;
-}
-
-//при нажатии на кнопку с карандашом открываем поп-ап
-profileBtnEdit.addEventListener('click', profileEdit);
-
-//////////////////////
-
-//сохранение текста из инпутов на страницу, часть кода с комментариями взята из тз, комментарии практикума сохранены
-// Находим форму в DOM
-const formInfoEdit = document.querySelector('.form_info-edit') // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-const nameInput = document.querySelector('.popup__input-edit_name') // Воспользуйтесь инструментом .querySelector()
-const jobInput = document.querySelector('.popup__input-edit_about')// Воспользуйтесь инструментом .querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-  function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    let job = document.querySelector('.profile__about');// Выберите элементы, куда должны быть вставлены значения полей
-    let name = document.querySelector('.profile__name');
-
-    job.textContent = jobInput.value;
-    name.textContent = nameInput.value;// Вставьте новые значения с помощью textContent
-    popupProfile.classList.remove('popup_opened');
-  }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formInfoEdit.addEventListener('submit', formSubmitHandler);
-
-//////////////////////
-
-//закрытие формы редактирования профиля со сбросом (нажатие Х)
-function formReset() {
-  document.querySelector('.form_info-edit').reset(); //сбрасываем значения в форме
-  popupProfile.classList.remove('popup_opened'); //убираем видимость попапа, удалив ему класс с видимостью
-}
-
-btnCloseProfile.addEventListener('click', formReset);
-
-//////////////////////
-
-//открытие попапа для добавления карточек
-
-function openCardsEditor() {
-  let popupAddCards = document.querySelector('.popup__new-place'); //выбираем попап добавления карточки
-  popupAddCards.classList.add('popup_opened'); //добавляем класс, чтобы попап был виден
-}
-
-//присваиваем кнопке "+" переменную
-let AddPlaceBtn = document.querySelector('.profile__button-add');
-
-//при нажатии на кнопку с "+" открываем поп-ап добавления карточек
-AddPlaceBtn.addEventListener('click', openCardsEditor);
-
-//закрытие формы редактирования профиля со сбросом (нажатие Х)
-function formResetPlace() {
-  document.querySelector('.form_add-place').reset(); //сбрасываем значения в форме
-  popupPlace.classList.remove('popup_opened'); //убираем видимость попапа, удалив ему класс с видимостью
-}
-
-btnClosePlace.addEventListener('click', formResetPlace);
-
-//////////////////////
-
-//добавление карточек
-
-function addCard(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
-  let placeNameInput = document.querySelector('.popup__input-edit_place').value; //значение названия в инпуте
-  let placeLinkInput = document.querySelector('.popup__input-edit_link').value; //ссылка на картинку в инпуте
-  let templateCard = document.querySelector('.template-card'); //темплэйт в конце html
-  let card = templateCard.content.cloneNode(true); //помещаем в переменную темплэйт клонированием всего содержимого
-  let image = card.querySelector('.cards__image'); //изображение внутри клонированного темплэйта
-  let cardsList = document.querySelector('.cards__list'); //список карточек
-  let cardHeader = card.querySelector('.cards__header'); //заголовок карточки
-
-  cardHeader.textContent = placeNameInput; //помещаем в заголовок карточки значение инпута с названием
-  image.src = placeLinkInput; //в атрибут src помещаем значение ссылки из инпута
-  image.alt = placeNameInput; //в alt помещаем название из инпута
-  cardsList.prepend(card); //помещаем новую карточку, созданную в темплэйте, в начало списка карточек
-  popupPlace.classList.remove('popup_opened');//закрытие формы при создании карточки
-}
-
-const formAddPlace = document.querySelector('.form_add-place');
-formAddPlace.addEventListener('submit', addCard);
-
-//////////////////////
-
-//открытие попапа с зумом картинки
-
-const popupImgZoom = document.querySelector('.popup__img-zoom');
-
-function zoomingImg(element) {
-  popupImgZoom.classList.add('popup_opened'); //добавляем класс, чтобы попап был виден
-//кладём в инпуты текущие значения со страницы
-  const imgZoomed = document.querySelector('.popup__img-zoomed'), //присваиваем
-    imgCaption = document.querySelector('figcaption'); //переменные
-
-  imgZoomed.src = element.target.src;
-  imgCaption.textContent = element.closest(document.querySelector('.cards__header')).textContent;
-
-  console.log(cardImg);
-}
-
-cardImg.addEventListener('click', zoomingImg);
-
-//закрытие попапа с зумом картинки
-
-const imgZoomCloseBtn = document.querySelector('.popup__close_img-zoom');
-
-function popupZoomClose() {
-  popupImgZoom.classList.remove('popup_opened');
-}
-
-imgZoomCloseBtn.addEventListener('click', popupZoomClose);
-
-//////////////////////
-
-const btnDelete = document.querySelector('.cards__delete');
-
-const handleClickBtnDelete = function(evt) {
-  const target = evt.target;
-  console.log(target);
-  const card = target.closest('.cards__card');
-  card.remove();
-}
-
-btnDelete.addEventListener('click', handleClickBtnDelete)
-//////////////////////
-
-const initialCards = [
+const cards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -180,6 +25,156 @@ const initialCards = [
   }
 ];
 
-function addStartCards(name, link) {
+const profileBtnEdit = document.querySelector('.profile__button-edit'), // кнопка редактирования профиля
+    popupProfile = document.querySelector('.popup__profile'), //окно редактирования профиля
+    btnCloseProfile = document.querySelector('.popup__close_profile'), //кнопка закрытия окна редактирования профиля
+    popupPlace = document.querySelector('.popup__new-place'), //попап окна добавления "мест"
+    btnClosePlace = document.querySelector('.popup__close_place'), //кнопка закрытия окна добавления мест
+    imgZoomCloseBtn = document.querySelector('.popup__close_img-zoom'), //кнопка закрытия увеличенной картинки
+    cardImg = document.querySelector('.cards__image'), //картинка в карточке
+    name = document.querySelector('.profile__name'), //имя в профиле
+    about = document.querySelector('.profile__about'), //"о себе" в профиле
+    nameInput = document.querySelector('.popup__input-edit_name'), //инпут "имя"
+    aboutInput = document.querySelector('.popup__input-edit_about'), //инпут "о себе"
+    formInfoEdit = document.querySelector('.form_info-edit'), //форма редактирование информации о пользователе
+    addPlaceBtn = document.querySelector('.profile__button-add'), //кнопка "+" для добавления карточек
+    placeNameInput = document.querySelector('.popup__input-edit_place'), //значение названия в инпуте
+    placeLinkInput = document.querySelector('.popup__input-edit_link'), //ссылка на картинку в инпуте
+    templateCard = document.querySelector('.template-card'), //темплэйт в конце html
+    cardsList = document.querySelector('.cards__list'), //список карточек
+    popupImgZoom = document.querySelector('.popup__img-zoom'), //попап зума картинок
+    imgZoomed = document.querySelector('.popup__img-zoomed'), //увеличенная картинка
+    imgCaption = document.querySelector('.popup__img-zoom-caption'), //подпись к картинке в попапе
+    formAddPlace = document.querySelector('.form_add-place'); //форма добавления карточек
 
+// функция открытия поп-апа. вместо popup в слушателях кнопок нужно будет указывать объявленные выше переменные конкретных попапов
+function openPopup(popup) {
+  popup.classList.add('popup_opened'); //добавляем класс, чтобы попап был виден
 }
+
+//в "значение"(value) инпутов кладём текущий текст со страницы
+function addProfileToInput() {
+  nameInput.value = name.textContent;
+  aboutInput.value = about.textContent;
+}
+
+//функция закрытия попапа, по аналогии с открытием, только заменяем "add" на "remove"
+function closePopup(popup) {
+  popup.classList.remove('popup_opened'); //добавляем класс, чтобы попап был виден
+}
+
+//при нажатии на кнопку с карандашом открываем поп-ап
+profileBtnEdit.addEventListener('click', () => {openPopup(popupProfile)}); //передаём в параметр popup попап редактирования профиля
+profileBtnEdit.addEventListener('click', addProfileToInput);
+
+//сохранение текста из инпутов на страницу, часть кода с комментариями взята из тз, комментарии практикума частично сохранены
+//upd вынес объявление переменных в начало файла
+// Находим форму в DOM
+  function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
+
+    // Получите значение полей aboutInput и nameInput из свойства value
+    about.textContent = aboutInput.value;
+    name.textContent = nameInput.value;// Вставьте новые значения с помощью textContent
+    // popupProfile.classList.remove('popup_opened');
+    closePopup(popupProfile);
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formInfoEdit.addEventListener('submit', formSubmitHandler);
+
+//закрытие формы (form) со сбросом класса попапа (popup): (нажатие Х)
+function formReset(form, popup) {
+  form.reset(); //сбрасываем значения инпутов в форме на те, что были при открытии
+  popup.classList.remove('popup_opened')
+}
+
+//закрытие формы редактирования профиля
+btnCloseProfile.addEventListener('click', () => formReset (formInfoEdit, popupProfile));
+
+//при нажатии на кнопку с "+" открываем поп-ап добавления карточек
+addPlaceBtn.addEventListener('click', () => {openPopup(popupPlace)});
+
+//при нажатии на "Х" закрывает попап добавления карточек
+btnClosePlace.addEventListener('click', () => formReset(formAddPlace, popupPlace));
+
+//удаление карточки
+const handleClickBtnDelete = function(evt) {
+  // const target = evt.target;
+  // const card = target.closest('.cards__card');
+  // card.remove();
+  evt.target.closest('.cards__card').remove();
+}
+
+//лайк
+const handleClickBtnLike = function (evt) {
+  evt.target.closest('.cards__like').classList.toggle('cards__like_active');
+}
+
+//открытие попапа с зумом картинки
+
+const handleClickBtnZoom = function(element) {
+  openPopup(popupImgZoom); //добавляем класс, чтобы попап был виден
+
+  imgZoomed.src = element.target.src;
+  imgCaption.textContent = element.target.alt;
+}
+
+//закрытие попапа с зумом картинки
+imgZoomCloseBtn.addEventListener('click', () => closePopup(popupImgZoom));
+
+//добавление 6 стартовых карточек из массива при загрузке страницы
+//добавление карточек должно быть после объявления функций, на которые внутри ссылаются слушатели событий
+const initiateCard = function(cardElement) {
+  // cardElement.preventDefault();
+  const card = templateCard.content.cloneNode(true), //помещаем в переменную темплэйт клонированием всего содержимого
+    image = card.querySelector('.cards__image'), //изображение внутри клонированного темплэйта
+    cardHeader = card.querySelector('.cards__header'), //заголовок карточки
+    btnDelete = card.querySelector('.cards__delete'), //кнопка удаления
+    like = card.querySelector('.cards__like'); //лайк на карточке
+
+  cardHeader.textContent = cardElement.name; //помещаем в заголовок карточки значение инпута с названием
+  image.src = cardElement.link; //в атрибут src помещаем значение ссылки из инпута
+  image.alt = cardElement.name; //в alt помещаем название из инпута
+  cardsList.prepend(card); //помещаем новую карточку, созданную в темплэйте, в начало списка карточек
+  closePopup(popupPlace);//закрытие формы при создании карточки
+
+  btnDelete.addEventListener('click', handleClickBtnDelete); //при добавлении новых элементов на страницу
+  like.addEventListener('click', handleClickBtnLike); //слушатель для лайка
+  image.addEventListener('click', handleClickBtnZoom);
+
+  return card; // https://efim360.ru/javascript-operator-return/
+}
+
+
+//сделано по аналогии с вебинаром
+const renderCard = function(data, container) {
+  const card = initiateCard(data);
+  container.prepend(card);
+}
+
+cards.forEach(function (item) {
+  renderCard(item, cardsList)
+})
+
+//добавление дополнительных карточек
+//переиспользование функции из инициализации карточек и добавить ".preventDefault()" тк при добавлении карточки страница обновляется
+formAddPlace.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  addCard();
+});
+
+
+function addCard() {
+  //изменение свойств name и link для "cardElement" объявляем переменную, помещаем в неё изменяемые свойства
+  //переменную используем в качестве аргумента для функции initiateCard
+  const parameters = {
+    name: placeNameInput.value,
+    link: placeLinkInput.value
+  };
+  initiateCard(parameters);
+}
+
